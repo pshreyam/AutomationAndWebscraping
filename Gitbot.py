e78146d8460208d8ca36850976f6b9b3903dda11
pshreyam <sp02434318@student.ku.edu.np>
2020-03-04 22:58:47 +0545

    Initial Commit

---
+++ Gitbot.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from secrets import usr,pwd,_chrome_path 
+import time 
+
+class GithubBot:
+    def __init__(self):
+        chrome_path=_chrome_path
+        self.driver=webdriver.Chrome(chrome_path)
+        self.driver.get("https://github.com")
+    
+    def login(self,username,password):
+        self._username=username
+        self._password=password
+        self.driver.find_element_by_xpath('/html/body/div[1]/header/div/div[2]/div[2]/a[1]').click()
+        self.driver.find_element_by_xpath('//*[@id="login_field"]').send_keys(self._username)
+        self.driver.find_element_by_xpath('//*[@id="password"]').send_keys(self._password)
+        self.driver.find_element_by_xpath('//*[@id="login"]/form/div[4]/input[9]').click()
+        print("Quit Driver")
+        self.driver.quit()
+
+
+gBot=GithubBot()
+gBot.login(usr,pwd)
